836cb755a8f9af6977086851edbda5c61f3ba7ef
Andre R. F. Batista <andrerfbatista@gmail.com>
2026-09-04 10:33:49 -0300

    ENH: Hurtbox with logs

--- GeneralNodes/HurtBox/hurt_box.gd
+++ GeneralNodes/HurtBox/hurt_box.gd
@@ -1,17 +1,31 @@
 class_name HurtBox extends Area2D
 
-@export var damage : int = 1 
+@export var damage : int = 1
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	area_entered.connect( AreaEntered )
-	pass # Replace with function body.
+	area_entered.connect(AreaEntered)
 
+func AreaEntered(a : Area2D) -> void:
+	print("--- [HURTBOX DETECTED] ---")
+	print("HurtBox belongs to: ", get_parent().name, " (Path: ", get_path(), ")")
+	print("Area entered: ", a.name, " (Path: ", a.get_path(), ")")
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-func AreaEntered ( a : Area2D) -> void:
 	if a is HitBox:
-		a.TakeDamege( damage )
+		print(">> Object detected IS A HitBox!")
+		print("HitBox belongs to: ", a.get_parent().name)
+
+		# Lock 1 : belongs to the same parent
+		if a.get_parent() == get_parent():
+			print(">> BLOCKED: HurtBox and HitBox belongs to the same parent (", get_parent().name, ")")
+			return
+
+		# Lock 2 : belongs to the same owner (should be a Player)
+		if owner != null and a.owner != null and a.owner == owner:
+			print(">> BLOCKED: HurtBox and HitBox belongs to the same owner (", owner.name, ")")
+			return
+
+		print(">> DAMAGED! HurtBox belongs to: ", get_parent().name)
+		a.TakeDamege(damage)
+	else:
+		print(">> The object detected is NOT a HitBox. Is a type: ", a.get_class())
+	print("---------------------------")
